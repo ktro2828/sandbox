9271cccf4a4fe7ae46beb17890e2b7be29d4d675
ktro2828 <kotaro.uetake@tier4.jp>
2022-09-13 16:07:06 +0900

    update boost
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- cpp/boost/boost_array.cpp
+++ cpp/boost/boost_array.cpp
@@ -9,5 +9,18 @@ int main()
 {
   boost::array<int, 3> ar = {3, 1, 4};
 
-  std::for_each(ar.begin(), ar.end(), disp);
+  // 配列の要素数
+  const std::size_t size = ar.size();
+  std::cout << size << std::endl;
+
+  // 添字による要素アクセス
+  std::cout << ar[0] << std::endl;
+
+  // イテレータ
+  std::for_each(ar.begin(), ar.end(), disp);  
+
+  boost::array<int, 3>::iterator it = std::find(ar.begin(), ar.end(), 1);
+  if (it != ar.end()) {
+    std::cout << *it << std::endl;
+  }
 }
